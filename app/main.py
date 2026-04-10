d9ee0de07f88b23578cc5bb6ee9f9c104b2a886e
Carlosraw <carlos.raw16@gmail.com>
2026-04-10 15:51:47 -0500

    se actualiza el main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from typing import Union
 
+from app.routers import libros
+from app.database import engine, Base
+from app.models.libro_db import LibroDB
+
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
+
+app.include_router(libros.router)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
